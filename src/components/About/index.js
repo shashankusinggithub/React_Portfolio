03a3a41f761e4e95a5928c2c59bd6e12c7ee1278
shashankusinggithub <shashank.chutke@gmail.com>
2022-05-22 18:13:14 +0530

    first commit

---
+++ src/components/About/index.js
@@ -0,0 +1,76 @@
+import { useEffect, useState } from 'react'
+import {
+  faAngular,
+  faCss3,
+  faGitAlt,
+  faHtml5,
+  faJsSquare,
+  faReact,
+  faPython,
+  faLinux
+} from '@fortawesome/free-brands-svg-icons'
+import Loader from 'react-loaders'
+import AnimatedLetters from '../AnimatedLetters'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import './index.scss'
+
+const About = () => {
+  const [letterClass, setLetterClass] = useState('text-animate')
+
+  useEffect(() => {
+    return setTimeout(() => {
+      setLetterClass('text-animate-hover')
+    }, 3000)
+  }, [])
+
+  return (
+    <>
+      <div className="container about-page">
+        <div className="text-zone">
+          <h1>
+            <AnimatedLetters
+              letterClass={letterClass}
+              strArray={['A', 'b', 'o', 'u', 't', ' ', 'm', 'e']}
+              idx={15}
+            />
+          </h1>
+          <p>
+          An Electronics and Communication engineering graduate with experience in IoT. One year back I started learning more about programming and software development and have never looked back since then.
+          </p>
+          <p align="LEFT">
+          The thing that I love most about software development is the fact that we can create about anything we would like to.
+          </p>
+          <p>
+          I am passionate about problem-solving and would like to utilize my skills in the area of computer software and programming.
+          </p>
+        </div>
+
+        <div className="stage-cube-cont">
+          <div className="cubespinner">
+            <div className="face1">
+              <FontAwesomeIcon icon={faHtml5} color="#DD0031" />
+            </div>
+            <div className="face2">
+              <FontAwesomeIcon icon={faPython} color="#F06529" />
+            </div>
+            <div className="face3">
+              <FontAwesomeIcon icon={faLinux} color="#28A4D9" />
+            </div>
+            <div className="face4">
+              <FontAwesomeIcon icon={faReact} color="#5ED4F4" />
+            </div>
+            <div className="face5">
+              <FontAwesomeIcon icon={faJsSquare} color="#EFD81D" />
+            </div>
+            <div className="face6">
+              <FontAwesomeIcon icon={faGitAlt} color="#EC4D28" />
+            </div>
+          </div>
+        </div>
+      </div>
+      <Loader type="pacman" />
+    </>
+  )
+}
+
+export default About
---
+++ src/components/Sidebar/index.js
@@ -0,0 +1,48 @@
+import './index.scss'
+import LogoS from '../../assets/images/S.png'
+import LogoSubtitle from '../../assets/images/shashank.png'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import linkedin from '../../assets/images/linkedin.png'
+import github from '../../assets/images/github.png'
+import { faHome, faUser, faEnvelope } from '@fortawesome/free-solid-svg-icons'
+import { Link, NavLink } from 'react-router-dom'
+
+const Sidebar = () => {
+  return (
+    <div className="nav-bar">
+      <Link className="logo" to="/">
+        <img src={LogoS} alt="Logo" />
+        <img className="sub-logo" src={LogoSubtitle} alt="slobodan" />
+      </Link>
+      <nav>
+        <NavLink exact="true" activeclassname="active" to="/">
+          <FontAwesomeIcon icon={faHome} color="#4d4d4e" />
+        </NavLink>
+        <NavLink activeclassname="active" className="about-link" to="/about">
+          <FontAwesomeIcon icon={faUser} color="#4d4d4e" />
+        </NavLink>
+        <NavLink
+          activeclassname="active"
+          className="contact-link"
+          to="/contact"
+        >
+          <FontAwesomeIcon icon={faEnvelope} color="#4d4d4e" />
+        </NavLink>
+      </nav>
+      <ul id='links'>
+            <li>
+                <a target="_blank" rel="noreferer" href='https://www.linkedin.com/in/shashank-c-8651971b6/'>
+                    <img src={linkedin} alt=" linkedin" ></img>
+                </a>
+            </li>
+            <li>
+                <a target="_blank" rel="noreferer" href='https://github.com/shashankusinggithub'>
+                    <img src={github} alt=" linkedin" ></img>
+                </a>
+            </li>
+        </ul>
+    </div>
+  )
+}
+
+export default Sidebar
